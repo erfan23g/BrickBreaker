1db52243ca28b829968cc1ad489cdd9a3914e5ee
erfanghorbani <erfan23g@icloud.com>
2024-03-24 19:24:38 +0330

    colors updated

--- src/ColorPalate.java
+++ src/ColorPalate.java
@@ -4,19 +4,19 @@ public class ColorPalate {
 
     public static Color GREEN = new Color(39, 174, 96);
     public static Color RED = new Color(231, 76, 60);
-    public static Color ORANGE = new Color(243, 156, 18);
-    public static Color BLUE = new Color(52, 152, 219);
-    public static Color DARK_GRAY = new Color(33,33,33);
-    public static Color GRAY = new Color(44,44,44);
-    public static Color LIGHT_GRAY = new Color(66,66,66);
-    public static Color LIGHT_GREEN = new Color(217, 237, 191);
-    public static Color LIGHT_BLUE = new Color(172, 226, 225);
-    public static Color GREEN2 = new Color(118, 171, 174);
+    public static Color YELLOW = new Color(240, 250, 0);
+    public static Color BLUE = new Color(52, 100, 255);
+    public static Color PURPLE = new Color(129,10,100);
+    public static Color DARK_GREEN = new Color(44,120,44);
+    public static Color PINK = new Color(240,66,200);
+    public static Color LIGHT_GREEN = new Color(190, 237, 19);
+    public static Color ORANGE = new Color(200, 100, 0);
+    public static Color CYAN = new Color(0, 200, 200);
 
 
 
     public static Color randomColor() {
-        Color[] choices =  {GREEN, RED, ORANGE, BLUE};
-        return choices[(int) (Math.random() * 4)];
+        Color[] choices =  {GREEN, RED, YELLOW, BLUE, PURPLE, DARK_GREEN, PINK, LIGHT_GREEN, ORANGE, CYAN};
+        return choices[(int) (Math.random() * 10)];
     }
 }
